5211df73aee1dc64046a850b883e61ebb6f9a4ff
etern4lslumb3r <jonasilagan173@gmail.com>
2022-07-26 15:29:13 +0800

    Update main.py
    - Encased main functions within CaesarCipher class.
    - Removed some hard coded features to increase modularity of program, hopefully paving way for custom alphabets

--- main.py
+++ main.py
@@ -5,45 +5,50 @@ from colorama import Style, Fore
 import replit
 
 replit.clear()
-characters_lower = string.ascii_lowercase
-characters_upper = string.ascii_uppercase
+
+class CaesarCipher:
+    characters_lower = string.ascii_lowercase
+    characters_upper = string.ascii_uppercase
 
 
-def encode_text(s: str, shift: int) -> str:
-    output = ""
-    for current_letter in s:
-        if current_letter not in characters_lower + characters_upper:
-            output += current_letter
+    def encode_text(self,s: str, shift: int) -> str:
+        output = ""
+        for current_letter in s:
+            if current_letter not in self.characters_lower + self.characters_upper:
+                output += current_letter
 
-        elif current_letter in characters_lower:
-            for char_index in range(len(characters_lower)):
-                if current_letter == characters_lower[char_index]:
-                    shift_amount = char_index + shift
-                    if shift_amount > len(characters_lower) - 1:
-                        shift_amount = shift_amount - len(characters_lower)
-                    new_char = characters_lower[shift_amount]
-                    output += new_char
+            elif current_letter in self.characters_lower:
+                for char_index in range(len(self.characters_lower)):
+                    if current_letter == self.characters_lower[char_index]:
+                        shift_amount = char_index + shift
+                        if shift_amount > len(self.characters_lower) - 1:
+                            shift_amount = shift_amount - len(self.characters_lower)
+                        new_char = self.characters_lower[shift_amount]
+                        output += new_char
 
-        elif current_letter in characters_upper:
-            for char_index in range(len(characters_upper)):
-                if current_letter == characters_upper[char_index]:
-                    shift_amount = char_index + shift
-                    if shift_amount > len(characters_lower) - 1:
-                        shift_amount = shift_amount - len(characters_lower)
-                    new_char = characters_upper[shift_amount]
-                    output += new_char
+            elif current_letter in self.characters_upper:
+                for char_index in range(len(self.characters_upper)):
+                    if current_letter == self.characters_upper[char_index]:
+                        shift_amount = char_index + shift
+                        if shift_amount > len(self.characters_lower) - 1:
+                            shift_amount = shift_amount - len(self.characters_lower)
+                        new_char = self.characters_upper[shift_amount]
+                        output += new_char
 
-    return output
+        return output
 
 
-def decode_text(s: str) -> List[str]:
-    outputs = []
-    for shift in reversed(range(26)):
-      output = encode_text(s, shift)
-      outputs.append(output)
-    return outputs
+    def decode_text(self, s: str) -> List[str]:
+        outputs = []
+        for shift in reversed(range(len(self.characters_lower))):
+            output = self.encode_text(s, shift)
+            outputs.append(output)
+        return outputs
+
+
 
 if __name__ == "__main__":
+    cs = CaesarCipher()
     while True:
         try:
             print("What would you like to do?")
@@ -67,8 +72,8 @@ if __name__ == "__main__":
                 try:
                     shift = int(
                         input(Fore.CYAN + "Shift text by: " + Style.RESET_ALL))
-                    if shift > 26:
-                        print(Fore.RED + "\nInput a number less than 26\n" + Style.RESET_ALL)
+                    if shift > len(cs.characters_lower):
+                        print(Fore.RED + f"\nInput a number less than {len(cs.characters_lower)}\n" + Style.RESET_ALL)
                         continue
                     else:
                         break
@@ -76,7 +81,7 @@ if __name__ == "__main__":
                     print(Fore.RED + "Input valid input" + Style.RESET_ALL)
                     continue
             print(Fore.CYAN + "Encoded text: " + Style.RESET_ALL +
-                  Fore.YELLOW + encode_text(input_text, shift) +
+                  Fore.YELLOW + cs.encode_text(input_text, shift) +
                   Style.RESET_ALL)
             input("\n[Press Enter to continue]\n")
 
@@ -89,8 +94,8 @@ if __name__ == "__main__":
                     continue
                 break
             print("\nPossibilities: \n")
-            for index, i in enumerate(decode_text(input_text)):
-                if index+1 == 26:
+            for index, i in enumerate(cs.decode_text(input_text)):
+                if index+1 == len(cs.characters_lower):
                     continue
                 print(Fore.YELLOW+f"[Shift: {index+1}]:  "+Style.RESET_ALL + i +"\n"+ Style.RESET_ALL)
             input("\n[Press Enter to continue]")
